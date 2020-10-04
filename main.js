7e7120aa682edb890f0fe3307cb212ff0278c48d
dudeamax99 <dudeamax99@yahoo.com>
2020-10-03 20:37:46 -0400

    I do not understand js.

--- main.js
+++ main.js
@@ -76,12 +76,10 @@
         }
     }
 
-/*  
   function c( t ) {
         t.keyCode === 32 && p();
     }
 
-*/
     function m( t ) {
         for ( var e, n = document.getElementById( t ), i = n.innerHTML.replace( "&amp;", "&" ).split( "" ), a = "", o = 0, s = i.length; s > o; o++ ) {
             e = i[ o ].replace( "&", "&amp" );
@@ -227,7 +225,7 @@
         document.addEventListener( "touchstart", i );
         document.body.addEventListener( "orientationchange", s );
         t.addEventListener( "resize", s );
-//        t.addEventListener( "keyup", c );
+        t.addEventListener( "keyup", c );
         t.addEventListener( "focus", r );
         t.addEventListener( "blur", h );
         s();
